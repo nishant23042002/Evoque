cd9cd3f15422e366364e9b53ab47204274a69e20
nishant23042002 <149838291+nishant23042002@users.noreply.github.com>
2025-12-07 16:58:35 +0530

    Added shadcn setup and intalled required UI components

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import React from "react";
 
 const Home = () => {
   return (
-    <div className="p-4 bg-accent-peach">
+    <div className="p-4 bg-accent-rose">
       <h1 className="text-xl font-semibold">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur iusto nihil rerum nisi modi. Nemo impedit, animi voluptatum quibusdam dolorum eum quis vitae modi, quam, maiores neque! Cumque, atque voluptatibus.</h1>
+      <Button size="lg">Click Me</Button>
     </div>
   )
 }
