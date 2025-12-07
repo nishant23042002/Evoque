40f3d41c1557663a1e96f429413ce4f940f8554a
nishant23042002 <149838291+nishant23042002@users.noreply.github.com>
2025-12-07 16:20:32 +0530

    Added color preset of pinterest

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>
+  return (
+    <div className="p-4 bg-accent-peach">
+      <h1 className="text-xl font-semibold">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur iusto nihil rerum nisi modi. Nemo impedit, animi voluptatum quibusdam dolorum eum quis vitae modi, quam, maiores neque! Cumque, atque voluptatibus.</h1>
+    </div>
+  )
 }
 
 export default Home;
